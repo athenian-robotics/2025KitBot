99ad6779008d7660f1defe25ad615d7eaf048a33
Non-Binary-Programmer <100527514+Non-Binary-Programmer@users.noreply.github.com>
2025-01-27 17:21:05 -0800

    update constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,7 +22,7 @@ import edu.wpi.first.units.*;
  */
 public final class Constants {
   public static final class DriveConstants {
-    public static final int LEFT_LEADER_ID = 1;
+    public static final int LEFT_LEADER_ID = 6;
     public static final int LEFT_FOLLOWER_ID = 2;
     public static final int RIGHT_LEADER_ID = 3;
     public static final int RIGHT_FOLLOWER_ID = 4;
