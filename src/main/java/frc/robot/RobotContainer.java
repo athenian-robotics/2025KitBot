c953d47d88a2d176ca89812addf13be8679d2a52
pi-wasnt-taken <n.randesi@gmail.com>
2025-12-05 17:18:27 -0800

    Started Circle Auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -46,7 +46,7 @@ public class RobotContainer {
     // Set the options to show up in the Dashboard for selecting auto modes. If you
     // add additional auto modes you can add additional lines here with
     // autoChooser.addOption
-    autoChooser.setDefaultOption("Autonomous", Autos.exampleAuto(driveSubsystem));
+    autoChooser.setDefaultOption("Autonomous", Autos.circleAuto(driveSubsystem));
   }
 
   /**
--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -4,12 +4,19 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CANDriveSubsystem;
 
 public final class Autos {
+  static Timer timer = new Timer();
+
   // Example autonomous command which drives forward for 1 second.
   public static final Command exampleAuto(CANDriveSubsystem driveSubsystem) {
     return driveSubsystem.driveArcade(driveSubsystem, () -> 0.5, () -> 0.0).withTimeout(1.0);
+  }
+
+  public static final Command circleAuto(CANDriveSubsystem driveSubsystem) {
+    return driveSubsystem.driveArcade(driveSubsystem, () -> timer.get(), () -> 0.5).withTimeout(10.0);
   }
 }
