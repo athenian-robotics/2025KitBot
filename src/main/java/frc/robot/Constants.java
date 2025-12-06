e1faa55fd5338e5db7746d8b0adc1ffc29cdc854
pi-wasnt-taken <n.randesi@gmail.com>
2025-12-05 16:04:05 -0800

    Slider speed adjustment for training

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,11 +21,11 @@ import edu.wpi.first.units.*;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static final class DriveConstants {
-    public static final int LEFT_LEADER_ID = 6;
-    public static final int LEFT_FOLLOWER_ID = 2;
-    public static final int RIGHT_LEADER_ID = 3;
-    public static final int RIGHT_FOLLOWER_ID = 4;
+  public static final class   DriveConstants {
+    public static final int LEFT_LEADER_ID = 3;
+    public static final int LEFT_FOLLOWER_ID = 4;
+    public static final int RIGHT_LEADER_ID = 2;
+    public static final int RIGHT_FOLLOWER_ID = 1;
 
     public static final Current DRIVE_MOTOR_CURRENT_LIMIT = Units.Amps.of(60);
   }
--- src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
@@ -11,6 +11,7 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -53,6 +54,7 @@ public class CANDriveSubsystem extends SubsystemBase {
     SparkMaxConfig config = new SparkMaxConfig();
     config.voltageCompensation(12);
     config.smartCurrentLimit((int) (Math.round(DriveConstants.DRIVE_MOTOR_CURRENT_LIMIT.in(Units.Amps))));
+    config.idleMode(IdleMode.kBrake);
 
     // Set configuration to follow leader and then apply it to corresponding
     // follower. Resetting in case a new controller is swapped
